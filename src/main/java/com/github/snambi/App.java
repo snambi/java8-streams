4d507ce151e5ff50f468434baeae49d1db5dfc71
Nambi Sankaran <snambi@gmail.com>
2016-11-28 16:26:26 -0800

    initial version

--- src/main/java/com/github/snambi/App.java
+++ src/main/java/com/github/snambi/App.java
@@ -1,9 +1,8 @@
 package com.github.snambi;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -13,36 +12,55 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Java 8 Streams benchmark" );
+
+        int count = 500;
+        if( args != null && args.length == 1 ){
+                // convert args to count
+                count = Integer.parseInt(args[0]);
+        }
+
+        App app = new App();
+
+        app.benchmark(count);
+    }
+
+    public void benchmark( int count ){
+
+        System.out.println("Number of iterations: "+ count);
 
         // first create a list
         List<String> words = new ArrayList<String>();
+        Queue queue = new ConcurrentLinkedQueue();
 
         // Add 500 words into the list
-        for( int i=0 ; i<100; i++ ){
+        for( int i=0 ; i<count; i++ ){
             words.add(UUID.randomUUID().toString() );
         }
 
         long begin1 = Calendar.getInstance().getTimeInMillis();
         for( String word: words ){
-            System.err.print( word);
-            System.err.print(" ");
+//            System.err.print( word);
+//            System.err.print(" ");
+            word.toLowerCase();
         }
         long end1 = Calendar.getInstance().getTimeInMillis();
 
-        System.err.println("\n time taken = "+ ( end1-begin1));
+        System.out.println("Java7 For Loop: time taken = "+ ( end1-begin1) + " ms");
 
         long begin2 = Calendar.getInstance().getTimeInMillis();
-        words.stream().forEach( System.err::print);
+        words.stream().forEach( e -> e.toLowerCase());
         long end2 = Calendar.getInstance().getTimeInMillis();
 
-        System.out.println("\n time taken = "+ ( end2-begin2));
+        System.out.println("Java8 Stream: time taken = "+ ( end2-begin2) + " ms");
 
 
         long begin3 = Calendar.getInstance().getTimeInMillis();
-        words.stream().parallel().forEach( System.err::print);
+        words.stream().parallel().forEach(e -> e.toLowerCase());
         long end3 = Calendar.getInstance().getTimeInMillis();
 
-        System.out.println("\n time taken = "+ ( end3-begin3));
+        System.out.println("Java8 Parallel Stream: time taken = "+ ( end3-begin3) + " ms");
+
+        System.out.println("Total number of items: "+ queue.size());
     }
 }
